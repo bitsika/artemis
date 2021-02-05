bfa2bd8a35c5bc32ea2fd9507715d0e0b417227a
ajibukunoluwa <ibukunajmoti@gmail.com>
2021-02-06 00:47:42 +0100

    Changed enum name to MerchantUserRole

---
+++ src/Enums/MerchantUserRole.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Bitsika\Artemis\Enums;
+
+
+final class MerchantUserRole 
+{
+    const SuperAdmin = 1;
+    const Administrator = 2;
+    const Developer = 3;
+    const Operation = 4;
+    const CustomerSupport = 5;
+    const Observer = 6;
+    const Suspended = 7;
+
+}
